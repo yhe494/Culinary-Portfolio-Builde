6386bfe5f258c8dccab21a907f90c78ae500d86c
VictorCentennial <ytse11@my.centennialcollege.ca>
2025-03-17 17:15:44 -0400

    Update passport configuration to use User model instead of Student

--- express-server/config/passport.js
+++ express-server/config/passport.js
@@ -2,19 +2,19 @@ const passport = require('passport');
 const JwtStrategy = require('passport-jwt').Strategy;
 const ExtractJwt = require('passport-jwt').ExtractJwt;
 const mongoose = require('mongoose');
-const Student = mongoose.model('Student');
-const config = require('./config'); 
+const User = mongoose.model('User');
+const config = require('./config');
 
 const opts = {
   jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-  secretOrKey: config.secretKey 
+  secretOrKey: config.secretKey
 };
 
 passport.use(new JwtStrategy(opts, async (jwt_payload, done) => {
   try {
-    const student = await Student.findById(jwt_payload.id);
-    if (student) {
-      return done(null, student);
+    const user = await User.findById(jwt_payload.id);
+    if (user) {
+      return done(null, user);
     } else {
       return done(null, false);
     }
