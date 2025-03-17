d3eb1e1bdc4f733962095881882ef2bea99601f4
VictorCentennial <ytse11@my.centennialcollege.ca>
2025-03-17 17:15:30 -0400

    Update Mongoose configuration to connect to user model

--- express-server/config/mongoose.js
+++ express-server/config/mongoose.js
@@ -1,18 +1,15 @@
 var config = require('./config'),
     mongoose = require('mongoose');
 //Define the Mongoose configuration method
-module.exports = function(){
+module.exports = function () {
     //Use Mongoose to connect to MongoDB
     const db = mongoose.connect(config.db, {
         useUnifiedTopology: true,
         useNewUrlParser: true
-    }).then(() => console.log('Connected to MongoDB...'))
-    .catch(err => {console.error('Could not connect to MongoDB...', err);});
+    }).then(() => console.log('Connected to MongoDB.'))
+        .catch(err => { console.error('Could not connect to MongoDB...', err); });
 
-    //Load the 'Student' model
-    require('../app/models/student.server.model');
-    //Load the 'Course' model
-    require('../app/models/course.server.model');
+    require('../app/models/user.server.model');
 
     return db;
 }
