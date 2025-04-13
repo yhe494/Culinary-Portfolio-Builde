2dddb33f283c3b4483d753c15b93ec9f4c9b90b3
vic70 <victortse70@gmail.com>
2025-04-13 02:01:17 -0400

    Update database environment variable to use lowercase 'db' to match with the .env

--- config/env/development.js
+++ config/env/development.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 module.exports = {
     port: process.env.PORT || 5001,
-    db: process.env.DB || 'mongodb://localhost/studentsandcourses-db',
+    db: process.env.db || 'mongodb://localhost/studentsandcourses-db',
     sessionSecret: process.env.SESSION_SECRET || 'developmentSessionSecret',
     secretKey: process.env.SECRET_KEY || 'real_secret'
 }; 
--- config/env/production.js
+++ config/env/production.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 module.exports = {
     port: process.env.PORT,
-    db: process.env.DB,
+    db: process.env.db,
     sessionSecret: process.env.SESSION_SECRET,
     secretKey: process.env.SECRET_KEY
 }; 
