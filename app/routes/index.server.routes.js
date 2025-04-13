bd01ff1026ee715a577031d7757a8992102cf72a
vic70 <victortse70@gmail.com>
2025-04-13 01:25:46 -0400

    Change root route to API endpoint in index server routes for deployment

--- app/routes/index.server.routes.js
+++ app/routes/index.server.routes.js
@@ -1,5 +1,5 @@
 var index = require('../controllers/index.server.controller')
 
-module.exports = function(app){
-    app.get('/', index.render);
+module.exports = function (app) {
+    app.get('/api', index.render);
 }
